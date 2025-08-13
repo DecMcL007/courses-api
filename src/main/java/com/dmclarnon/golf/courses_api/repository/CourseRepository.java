19fbb11ea89aa2d173495252df110ec4b47f5865
D McLarnon <dmclarnon@Ds-Mac-mini.local>
2025-08-13 16:51:28 +0100

    Just removed some commented sql not needed

--- src/main/java/com/dmclarnon/golf/courses_api/repository/CourseRepository.java
+++ src/main/java/com/dmclarnon/golf/courses_api/repository/CourseRepository.java
@@ -5,16 +5,6 @@ import org.springframework.data.domain.*;
 import org.springframework.data.jpa.repository.*;
 
 public interface CourseRepository extends JpaRepository<Course, Long> {
-    /*
-    @Query("""
-     SELECT c FROM Course c
-     WHERE (:q IS NULL OR LOWER(c.name) LIKE LOWER(CONCAT('%', :q, '%'))
-                     OR LOWER(c.location) LIKE LOWER(CONCAT('%', :q, '%')))
-  """)
-    Page<Course> search(String q, Pageable pageable);
-    */
-
-
         Page<Course> findByNameContainingIgnoreCaseOrLocationContainingIgnoreCase(
                 String name, String location, Pageable pageable);
 
